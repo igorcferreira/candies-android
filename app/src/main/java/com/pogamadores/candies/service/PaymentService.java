fc3e1d236c1e6d46cc4690b894ad61ea407491fe
Igor Ferreira <igorcferreira@hotmail.com>
2015-01-19 12:13:51 -0200

    Merge branch 'master' of https://github.com/jeffprestes/candies-android into jeffprestes-master
    Conflicts:
    	app/build.gradle
    	app/src/main/AndroidManifest.xml
    	app/src/main/java/com/pogamadores/candies/service/PaymentService.java
    	app/src/main/java/com/pogamadores/candies/util/Util.java

--- app/src/main/java/com/pogamadores/candies/service/PaymentService.java
+++ app/src/main/java/com/pogamadores/candies/service/PaymentService.java
@@ -1,5 +1,6 @@
 package com.pogamadores.candies.service;
 
+import android.app.Application;
 import android.app.Service;
 import android.content.Intent;
 import android.os.Binder;
@@ -16,56 +17,55 @@ import com.pogamadores.candies.domain.Token;
 import com.pogamadores.candies.util.Util;
 import com.pogamadores.candies.util.WebServerHelper;
 
+import java.util.Calendar;
+
+
+
 public class PaymentService extends Service {
 
     private LocalBinder mBinder = new LocalBinder();
     private PaymentStepsListener paymentStepsListener;
+    private Token token = null;
 
-    public PaymentService() {}
+    public interface PaymentStepsListener {
+        public void onPaymentStarted(Token token, double value);
+        public void onPaymentFinished(Token token, double value, boolean successful, String message);
+    }
+
+
+    public PaymentService() {
+        token = CandiesApplication.getDatasource().getToken();
+    }
+
+    private String getStringToken()    {
+        return token.getToken();
+    }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        Toast.makeText(getApplicationContext(), "Pagamento", Toast.LENGTH_SHORT).show();
+
         final Token token = CandiesApplication.getDatasource().getToken();
+
         if(token != null) {
-
             NotificationManagerCompat.from(getApplicationContext()).cancel(Util.NOTIFICATION_ID);
-
-            WebServerHelper.performNewPayment(
-                    token.getToken(),
+            WebServerHelper.performNewPayment (
+                    this.getStringToken(),
                     Util.PRODUCT_DEFAULT_VALUE,
-                    new Response.Listener<NewTransaction>() {
-                        @Override
-                        public void onResponse(NewTransaction response) {
-                            if (response.isSuccessfull()) {
-                                Toast.makeText(getApplicationContext(), "Sucesso", Toast.LENGTH_SHORT).show();
-                                if(paymentStepsListener != null)
-                                    paymentStepsListener.onPaymentFinished(token, Util.PRODUCT_DEFAULT_VALUE, true, response.getMessage());
-                            } else {
-                                Toast.makeText(getApplicationContext(), response.getMessage(), Toast.LENGTH_SHORT).show();
-                                if(paymentStepsListener != null)
-                                    paymentStepsListener.onPaymentFinished(token, Util.PRODUCT_DEFAULT_VALUE, false,response.getMessage());
-                            }
-                            stopSelf();
-                        }
-                    },
-                    new Response.ErrorListener() {
-                        @Override
-                        public void onErrorResponse(VolleyError error) {
-                            Log.e(CandiesApplication.class.getSimpleName(), Log.getStackTraceString(error));
-                            Toast.makeText(getApplicationContext(), "Error on payment", Toast.LENGTH_SHORT).show();
-                            if(paymentStepsListener != null)
-                                paymentStepsListener.onPaymentFinished(token, Util.PRODUCT_DEFAULT_VALUE, false, error.getMessage());
-                            stopSelf();
-                        }
-                    }
+                    this.getResponsePaymentListener(),
+                    this.getErrorPaymentListener()
             );
-            if(paymentStepsListener != null)
+
+            if(paymentStepsListener != null) {
                 paymentStepsListener.onPaymentStarted(token, Util.PRODUCT_DEFAULT_VALUE);
+            }
+
             return START_STICKY;
-        } else
+
+        } else {
             return START_NOT_STICKY;
+        }
     }
+
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -76,14 +76,54 @@ public class PaymentService extends Service {
         this.paymentStepsListener = paymentStepsListener;
     }
 
-    public interface PaymentStepsListener {
-        public void onPaymentStarted(Token token, double value);
-        public void onPaymentFinished(Token token, double value, boolean successful, String message);
-    }
 
     public class LocalBinder extends Binder {
         public PaymentService getService() {
             return PaymentService.this;
         }
+    }
+
+
+    /**
+     * Return specific Response Payment listener to payment webservice call
+     */
+    public Response.Listener<NewTransaction> getResponsePaymentListener() {
+
+        return new Response.Listener<NewTransaction>() {
+
+            public void onResponse (NewTransaction response)    {
+
+                if (response.isSuccessfull()) {
+                    Util.sendMessageToMachine("release");
+                    Toast.makeText(getApplicationContext(), "Doce sendo liberado...", Toast.LENGTH_SHORT).show();
+                    if (paymentStepsListener != null)
+                        paymentStepsListener.onPaymentFinished(token, Util.PRODUCT_DEFAULT_VALUE, true, response.getMessage());
+
+                } else {
+                    Toast.makeText(getApplicationContext(), response.getMessage(), Toast.LENGTH_SHORT).show();
+                    if (paymentStepsListener != null)
+                        paymentStepsListener.onPaymentFinished(token, Util.PRODUCT_DEFAULT_VALUE, false, response.getMessage());
+                }
+                stopSelf();
+            }
+
+        };
+    }
+
+
+    /**
+     * Specific error listener for payment webservice call
+     * @return Error Listener for payment webservice call
+     */
+    public Response.ErrorListener getErrorPaymentListener() {
+        return new Response.ErrorListener() {
+            public void onErrorResponse(VolleyError error) {
+                Log.e(CandiesApplication.class.getSimpleName(), Log.getStackTraceString(error));
+                Toast.makeText(getApplicationContext(), "Erro no pagamento: " + error.getLocalizedMessage() + " | Tempo final: " + (Calendar.getInstance().getTimeInMillis()), Toast.LENGTH_SHORT).show();
+                if (paymentStepsListener != null)
+                    paymentStepsListener.onPaymentFinished(token, Util.PRODUCT_DEFAULT_VALUE, false, error.getMessage());
+                stopSelf();
+            }
+        };
     }
 }
--- app/src/main/java/com/pogamadores/candies/util/Util.java
+++ app/src/main/java/com/pogamadores/candies/util/Util.java
@@ -11,14 +11,20 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.wearable.PutDataMapRequest;
 import com.google.android.gms.wearable.Wearable;
 import com.pogamadores.candies.R;
+import com.pogamadores.candies.application.CandiesApplication;
 import com.pogamadores.candies.broadcast.PaymentOrderReceiver;
 
 import org.altbeacon.beacon.Beacon;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
 
 public class Util
 {
@@ -88,5 +94,41 @@ public class Util
         putDataMapRequest.getDataMap().putString("content",builder.toString());
 
         Wearable.DataApi.putDataItem(client, putDataMapRequest.asPutDataRequest());
+    }
+
+    public static String getPhoneIMEI(Context context)     {
+        TelephonyManager telephonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+        return telephonyManager.getDeviceId();
+    }
+
+
+    /**
+     * Sends message to machine via MQTT Protocol
+     * @return results of operation. True if message has been sent or false in failure.
+     */
+    public static boolean sendMessageToMachine(String message)     {
+
+        CandiesApplication app = CandiesApplication.get();
+
+        MqttClient mqttClient = app.getMqttClient();
+
+        boolean retorno = false;
+
+        try {
+            mqttClient.connect();
+            if (mqttClient.isConnected()) {
+                mqttClient.publish("jeffprestes/candies/world", new MqttMessage(message.getBytes()));
+                mqttClient.disconnect();
+                retorno = true;
+            }
+
+        } catch (Exception e) {
+            //TODO: Colocar mensagens no arquivo Strings
+            Toast.makeText(CandiesApplication.get().getApplicationContext(), "Nao foi possivel fazer a comunicacao com a maquina. Erro: " + e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+            Log.e("JEFFDEBUG", "Erro ao enviar mensagem a maquina: " + e.getLocalizedMessage(), e);
+
+        } finally {
+            return retorno;
+        }
     }
 }
